3a7e0bb7fc026bbc4ee26a8134567cb095ad5b6b
Morten Ørris Poulsen <morten.oerris.poulsen@LEGO.com>
2018-12-18 15:46:55 +0100

    Added ARules files from github. Updated to prevent failing under Julia 1.0

---
+++ ARules/README.md
@@ -0,0 +1,14 @@
+ARules.jl
+=========
+
+Julia package for Association Rule Learning algorithms.
+
+## Current Supported Algorithms
+
+### Apriori (with rule generation algorithm)
+
+The Apriori implementation is based on the book *Introduction to Data Mining*, written by Pang-Ning Tan, et al. A Fₖ₋₁ × Fₖ₋₁ method was used on Frequent itemset candidates generation.
+
+## Planned
+
+* FP-growth
---
+++ MLwR_8.jl
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------
+# Julia implementation of the R code from
+#
+# Machine Learning with R
+# Chapter 8, Finding Patterns – Market Basket Analysis Using Association Rules
+# --------------------------------------------------------------------------
+
+
+# Setup --------------------------------------------------------------------
+include("ARules/src/ARules.jl")
